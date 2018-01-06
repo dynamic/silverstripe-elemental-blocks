36ce6e1b5e1a6cff97c3c8e2c459e8beef94507a
Jason Irish <jsirish@users.noreply.github.com>
2018-01-06 15:19:48 -0600

    ElementContent - additional fields (#43)
    closes #38

---
+++ src/Extensions/ElementContentDataExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dynamic\Elements\ORM;
+
+use Sheadawson\Linkable\Forms\LinkField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+class ElementContentDataExtension extends DataExtension
+{
+    /**
+     * @var array
+     */
+    private static $has_one = [
+        'Image' => Image::class,
+        'ElementLink' => Link::class,
+    ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->replaceField(
+            'ElementLinkID',
+            LinkField::create('ElementLinkID')
+                ->setTitle('Link')
+                ->setDescription('Optional. Add a call to action link.')
+        );
+        $fields->insertBefore($fields->dataFieldByName('ElementLinkID'), 'HTML');
+
+            $image = $fields->dataFieldByName('Image')
+                ->setDescription('Optional. Display an image with this content.')
+                ->setFolderName('Uploads/Elements/Content');
+            $fields->insertBefore($image, 'HTML');
+    }
+}
