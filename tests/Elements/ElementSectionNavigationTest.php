84792d5269fff98f73648ab5cbc77176394b5543
Jason Irish <jirish@dynamicagency.com>
2018-01-06 18:27:35 -0600

    test coverage

--- tests/Elements/ElementSectionNavigationTest.php
+++ tests/Elements/ElementSectionNavigationTest.php
@@ -5,6 +5,7 @@ namespace Dynamic\Elements\Tests;
 use Dynamic\Elements\Elements\ElementSectionNavigation;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\FieldType\DBField;
 
 /**
  * Class ElementSectionNavigationTest.
@@ -20,5 +21,24 @@ class ElementSectionNavigationTest extends SapphireTest
         $this->assertFalse($nav->getSectionNavigation());
 
         // TODO - figure out how to mock up element areas
+    }
+
+    /**
+     *
+     */
+    public function testGetElementSummary()
+    {
+        $object = Injector::inst()->create(ElementSectionNavigation::class);
+        $expected = DBField::create_field('HTMLText', '<p>Section Navigation</p>')->Summary(20);
+        $this->assertEquals($object->ElementSummary(), $expected);
+    }
+
+    /**
+     *
+     */
+    public function testGetType()
+    {
+        $object = Injector::inst()->create(ElementSectionNavigation::class);
+        $this->assertEquals($object->getType(), 'Section Navigation');
     }
 }
